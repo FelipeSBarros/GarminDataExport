b23d8d9485137f3b28dad0cdce389698b79e3507
Felipe Barros <felipe.b4rros@gmail.com>
2018-08-14 22:47:24 -0300

    remove all unnecessary files

--- SeleniumChromeTest.py
+++
@@ -1,8 +0,0 @@
-from pyvirtualdisplay import Display
-from selenium import webdriver
-
-display = Display(visible=0, size=(800, 600))
-display.start()
-driver = webdriver.Chrome()
-driver.get('https://connect.garmin.com/')
-print(driver.title)
